f18b8bc23cef57a5ab46d3f1122828ab93ba8804
Albin Alm <almalbin98@gmail.com>
2021-03-18 10:06:28 +0100

    Added ShipSize class and Size property. Added StarWarsAPI reference to StarWarsTerminal.csproj

--- Source/StarWarsApi/Models/SpaceShip.cs
+++ Source/StarWarsApi/Models/SpaceShip.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,21 @@ namespace StarWarsApi.Models
     public class SpaceShip
     {
         public string Name { get; set; }
-        public long Weight { get; set; }
-        public long Length { get; set; }
-        public long Width { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public ShipSize Size { get; set; } 
+        //Noticed SWAPI JSON doesn't contain values for height or width. Will we use our own measurements?
+        public class ShipSize
+        {
+            public readonly long Width;
+            public readonly long Length;
+            public readonly long Height;
+            public ShipSize(long width, long length, long height)
+            {
+                Width = width;
+                Length = length;
+                Height = height;
+            }
+        }
     }
 }
--- Source/StarWarsTerminal/Main/Program.cs
+++ Source/StarWarsTerminal/Main/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+using StarWarsApi.Models;
 namespace StarWarsTerminal
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SpaceShip spaceShip = new SpaceShip();
+            spaceShip.Size = new SpaceShip.ShipSize(50, 200, 20);
+            Console.WriteLine(
+                $"Width: {spaceShip.Size.Width}\nLength: {spaceShip.Size.Length}\nHeight: {spaceShip.Size.Height}");
         }
     }
 }
