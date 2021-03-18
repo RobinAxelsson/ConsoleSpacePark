652f4ec2481167e2d222d376aeeca322597cba79
Albin Alm <almalbin98@gmail.com>
2021-03-18 14:02:54 +0100

    Added APICollector

--- Source/StarWarsApi/Models/User.cs
+++ Source/StarWarsApi/Models/User.cs
@@ -10,5 +10,21 @@ namespace StarWarsApi.Models
     {
         public string Name { get; set; }
         public int ID { get; set; }
+        public string Gender { get; set; }
+        public string Eye_color { get; set; }
+        public UserSpecies Species { get; set; }
+        public User(string name, int id, UserSpecies species)
+        {
+            Name = name;
+            ID = id;
+            Species = species;
+        }
+
+        public class UserSpecies
+        {
+            public readonly string Name;
+            public readonly string Classification;
+            public readonly string Language;
+        }
     }
 }
