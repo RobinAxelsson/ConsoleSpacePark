6beeb8a4c0bc68c69cc4efaf07e57e082785a32b
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-19 12:44:03 +0100

    ASCII-art user interface, welcomescreen and form

---
+++ Source/StarWarsTerminal/UI/ConsoleWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace StarWarsTerminal.UI
+{
+    public static class ConsoleWriter
+    {
+        public static void Write(IDrawable drawable)
+        {
+            //if (drawable.IsDrawn == true) return;
+            Console.ForegroundColor = drawable.ForegroundColor;
+            Console.BackgroundColor = drawable.BackgroundColor;
+            Console.SetCursorPosition(drawable.CoordinateX, drawable.CoordinateY);
+            Console.Write(drawable.Chars);
+            drawable.IsDrawn = true;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void Erase(IDrawable drawable)
+        {
+            if (drawable.IsDrawn == false) return;
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(drawable.CoordinateX, drawable.CoordinateY);
+            Console.Write(" ");
+            drawable.IsErased = true;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Source/StarWarsTerminal/UI/TextDrawable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StarWarsTerminal.UI
+{
+    public class TextDrawable : IDrawable
+    {
+        public int CoordinateX { get; set; }
+        public int CoordinateY { get; set; }
+        public string Chars { get; set; }
+        public ConsoleColor BackgroundColor { get; set; } = ConsoleColor.Black;
+        public ConsoleColor ForegroundColor { get; set; } = ConsoleColor.White;
+        public bool IsDrawn { get; set; }
+        public bool IsErased { get; set; }
+
+        public bool IsSame(IDrawable drawable) { return false; }
+    }
+}
