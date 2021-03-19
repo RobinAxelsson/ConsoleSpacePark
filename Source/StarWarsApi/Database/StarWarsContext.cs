1275e003c5ebba30f03b1fb67d85e15121c03bb1
Albin Alm <almalbin98@gmail.com>
2021-03-19 12:56:05 +0100

    Removed stuff

--- Source/StarWarsApi/Database/StarWarsContext.cs
+++ Source/StarWarsApi/Database/StarWarsContext.cs
@@ -14,7 +14,7 @@ namespace StarWarsApi.Database
         public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsbuilder)
         {
-            optionsbuilder.UseSqlServer(@"Data Source=90.229.161.68,1433;Initial Catalog=StarWarsDataBase2.0;user id=adminuser;password=starwars");
+            optionsbuilder.UseSqlServer(@"");
         }
     }
 }
