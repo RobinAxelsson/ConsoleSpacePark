3f517a8359ec0428c945c8956c4e008806c1ae9d
coachvredin <43240053+coachvredin@users.noreply.github.com>
2021-03-22 15:15:50 +0100

    Small changes

--- Source/StarWarsApi/Database/StarWarsContext.cs
+++ Source/StarWarsApi/Database/StarWarsContext.cs
@@ -12,6 +12,13 @@ namespace StarWarsApi.Database
     {
         public DbSet<SpaceShip> SpaceShips { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Hangar> Hangars { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<User.Homeworld> Homeworlds { get; set; }
+
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsbuilder)
         {
             optionsbuilder.UseSqlServer(@"");
--- Source/StarWarsApi/Models/Receipt.cs
+++ Source/StarWarsApi/Models/Receipt.cs
@@ -1,8 +1,8 @@
 namespace StarWarsApi.Models
 {
-    public class Reciept
+    public class Receipt
     {
-        public int RecieptID { get; set; }
+        public int ReceiptID { get; set; }
         public Account Account { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
