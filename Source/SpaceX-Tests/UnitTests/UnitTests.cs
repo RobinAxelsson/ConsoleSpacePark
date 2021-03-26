f95902d3a9220e95d3e1cb5cd7a291929bc2f921
coachvredin <43240053+coachvredin@users.noreply.github.com>
2021-03-26 16:38:24 +0100

    Added tests

---
+++ Source/SpaceX-Tests/UnitTests/UnitTests.cs
@@ -0,0 +1,64 @@
+﻿using StarWarsApi.Models;
+using StarWarsApi.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static StarWarsApi.Database.DatabaseManagement;
+
+namespace SpaceX_Tests.UnitTests
+{
+    public class DbTests
+    {
+        [Fact]
+        public void ParseUserAsync_RightInput_ExpectBlue()
+        {
+            var user = APICollector.ParseUserAsync("Luke Skywalker");
+
+            string expectedEyeColor = "blue";
+
+            Assert.True(expectedEyeColor == user.eyeColor);
+            //Assert.Equal(expectedEyeColor, user.eyeColor);
+        }
+
+        [Fact]
+        public void ParseShipAsync_RightInput_ExpectCorellia()
+        {
+            var ship = APICollector.ParseShipAsync("YT-1300 light freighter");
+
+            string expectedManufacturer = "Corellian Engineering Corporation";
+
+            Assert.True(expectedManufacturer == ship.Manufacturer);
+        }
+
+        [Fact]
+        public void CalculatePrice_RightInput_ExpectPricePerMinuteForCorvette()
+        {
+            var pm = new ParkingManagement();
+
+            var ship = APICollector.ParseShipAsync("CR90 corvette");
+
+            decimal price = pm.CalculatePrice(ship, 1);
+
+            decimal expectedPrice = 15;
+
+            Assert.Equal(expectedPrice, price);
+        }
+
+        [Fact]
+        public void DoesUserExists_RightInput_ExpectLukeSkywalker()
+        {
+            ConnectionString = @"Server = 90.229.161.68,52578; Database = StarWarsProject2.3; User Id = adminuser; Password = starwars;";
+
+            var am = new AccountManagement();
+            
+            var user = APICollector.ParseUserAsync("Darth Maul");
+
+            var expectedResult = am.Exists(user.Name);
+
+            Assert.True(expectedResult);
+        }
+    }
+}
