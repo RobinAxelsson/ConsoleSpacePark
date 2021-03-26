407e31d19c99b6970788b2e870da28409afead1a
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-26 11:00:54 +0100

    added parkingavailable bool with loop

--- Source/StarWarsTerminal/Main/TestMain.cs
+++ Source/StarWarsTerminal/Main/TestMain.cs
@@ -11,10 +11,70 @@ namespace StarWarsTerminal.Main
     {
         static void Main(string[] args)
         {
-            //CreateUser();
+            DatabaseManagement.ConnectionString = @"Server = 90.229.161.68,52578; Database = StarWarsProject2.6; User Id = adminuser; Password = starwars;";
+
             var account = LoginDarthVader();
-            BuyTicket(account, 5);
+
+            while (IsParkingAvailable())
+                BuyTicket(account, 5);
+
+            ViewReceipts(account);
             Console.ReadLine();
+        }
+        public static bool IsParkingAvailable()
+        {
+            var parking = new DatabaseManagement.ParkingManagement();
+            return parking.CheckParkingStatus().isOpen;
+        }
+        public static void ConsoleIsParkingAvailable()
+        {
+            var parking = new DatabaseManagement.ParkingManagement();
+            var response = parking.CheckParkingStatus();
+            if (response.isOpen) Console.WriteLine("Parking available");
+            else Console.WriteLine("parking is available at: " + response.nextAvailable);
+        }
+        public static void ViewReceipts(Account account)
+        {
+            var am = new DatabaseManagement.AccountManagement();
+            var receipts = am.GetAccountReceipts(account);
+            foreach (var receipt in receipts)
+            {
+                string[] receiptString = new string[]
+                {
+                    "Ticket Holder: " + receipt.Account.AccountName,
+                    "Start time: " + receipt.StartTime,
+                    "End time: " + receipt.EndTime,
+                    "Price: " + receipt.Price
+                };
+                string test = String.Join('\n', receiptString);
+
+                Console.Write(test + '\n' + '\n');
+            }
+        }
+        public static void BuyTicket(Account account, int hours)
+        {
+
+            var parking = new DatabaseManagement.ParkingManagement();
+            var receipt = parking.SendInvoice(account, hours);
+
+            string[] receiptString = new string[]
+                {
+                    "Ticket Holder: " + receipt.Account.AccountName,
+                    "Start time: " + receipt.StartTime,
+                    "End time: " + receipt.EndTime,
+                    "Price: " + receipt.Price
+                };
+            string test = String.Join('\n', receiptString);
+            Console.Write(test);
+        }
+        public static Account LoginDarthVader()
+        {
+            string accountName = "darth123";
+            string password = "darth123";
+            var accountManagement = new DatabaseManagement.AccountManagement();
+            var account = accountManagement.ValidateLogin(accountName, password);
+            if (account == null) throw new Exception("Login didn't work");
+            return account;
         }
         public static Account Login(string accountName, string password)
         {
@@ -78,49 +138,6 @@ namespace StarWarsTerminal.Main
             var response = parking.CheckParkingStatus();
             if (response.isOpen) Console.WriteLine("Parking available");
             else Console.WriteLine("parking is available at: " + response.nextAvailable);
-        }
-        public static void ViewReceipts(Account account)
-        {
-            var am = new DatabaseManagement.AccountManagement();
-            var receipts = am.GetAccountReceipts(account);
-            foreach (var receipt in receipts)
-            {
-                string[] receiptString = new string[]
-                {
-                    "Ticket Holder: " + receipt.Account.AccountName,
-                    "Start time: " + receipt.StartTime,
-                    "End time: " + receipt.EndTime,
-                    "Price: " + receipt.Price
-                };
-                string test = String.Join('\n', receiptString);
-
-                Console.Write(test + '\n' + '\n');
-            }
-        }
-        public static void BuyTicket(Account account, int hours)
-        {
-           
-            var parking = new DatabaseManagement.ParkingManagement();
-            var receipt = parking.SendInvoice(account, hours);
-
-            string[] receiptString = new string[]
-                {
-                    "Ticket Holder: " + receipt.Account.AccountName,
-                    "Start time: " + receipt.StartTime,
-                    "End time: " + receipt.EndTime,
-                    "Price: " + receipt.Price
-                };
-            string test = String.Join('\n', receiptString);
-            Console.Write(test);
-        }
-        public static Account LoginDarthVader()
-        {
-            string accountName = "darth123";
-            string password = "darth123";
-            var accountManagement = new DatabaseManagement.AccountManagement();
-            var account = accountManagement.ValidateLogin(accountName, password);
-            if (account == null) throw new Exception("Login didn't work");
-            return account;
         }
     }
 }
